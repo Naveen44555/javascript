313343fb309f981a373d0cc650a4cba7289db0b1
Naveen44555 <naveennaveenyadav386@gmail.com>
2025-11-20 09:32:04 +0530

    calculator

--- ddd.py
+++ ddd.py
@@ -1,3 +1,13 @@
 print("\033[31mNaveen\033[0m")  # Red
 print("\033[32mNaveen\033[0m")  # Green
 print("\033[34mNaveen\033[0m")  # Blue
+
+print("hello")
+# a="val"
+# b=int(a)
+# print(type(b))
+
+a={"name":"naveen"}
+b={"village":"dcl"}
+c={*b,*a}
+print(c)

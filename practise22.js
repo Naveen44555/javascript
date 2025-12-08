f7d27fc40e213650c112c17a5d6ace1ae29b7c29
Naveen44555 <naveennaveenyadav386@gmail.com>
2025-12-08 16:12:20 +0530

    upd

---
+++ practise22.js
@@ -0,0 +1,8 @@
+// destructing
+let a=[22,"naveen","dcl",5.4,"2acres"]
+var b=[age,name,place,innerHeight,land]=a
+console.log(b)
+console.log(name)
+console.log(land)
+document.write("land : ",land)
+
